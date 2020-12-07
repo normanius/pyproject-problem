9910bdec4dfe3e326b1f6039360ab174eb410d35
Norman <normanius@gmail.com>
2020-12-07 18:14:12 +0100

    Some corrections for a better demonstration of the problem

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
+_CODE=
+_CODE="${_CODE}import hello_world_normanius as hello;"
+_CODE="${_CODE}print('>>> Current version:', hello.__version__)"
 
+echo
+echo
 echo "###########################################################"
 echo "# TEST 1: FAILURE"
 echo "###########################################################"
@@ -17,17 +22,23 @@ echo ">>> deploying package to TestPyPI (requires an account)"
 twine upload --repository testpypi dist/*
 
 echo
-echo ">>> Sleeping a 20s..."
-sleep 20s
+echo ">>> Sleeping for 20s..."
+sleep 30s
 
 echo
 echo ">>> Install v${_VERSION} via TestPyPI (failure)"
 rm -rf hello_world_normanius.egg-info
 pip uninstall -y hello_world_normanius
+# Check twice, the first time the upload usually isn't available yet.
 pip install --index-url https://test.pypi.org/simple/ \
             --no-deps hello_world_normanius==$_VERSION
+pip install --index-url https://test.pypi.org/simple/ \
+            --no-deps hello_world_normanius==$_VERSION
+# python -c "$_CODE"
 
 
+echo
+echo
 echo "###########################################################"
 echo "# TEST 2: FIX BY REMOVING pyproject.toml"
 echo "###########################################################"
@@ -48,17 +59,22 @@ echo ">>> deploying package to TestPyPI (requires an account)"
 twine upload --repository testpypi dist/*
 
 echo
-echo ">>> Sleeping a 20s..."
-sleep 20s
+echo ">>> Sleeping for 20s..."
+sleep 30s
 
 echo
 echo ">>> Install v$_VERSION via TestPyPI (now it works)"
 rm -rf hello_world_normanius.egg-info
 pip uninstall -y hello_world_normanius
+# Check twice, the first time the upload usually isn't available yet.
 pip install --index-url https://test.pypi.org/simple/ \
             --no-deps hello_world_normanius==$_VERSION
+pip install --index-url https://test.pypi.org/simple/ \
+            --no-deps hello_world_normanius==$_VERSION
+python -c "$_CODE"
 
-
+echo
+echo
 echo "###########################################################"
 echo "# TEST 3: FIX BY LOCAL INSTALLATION"
 echo "###########################################################"
@@ -77,3 +93,4 @@ echo ">>> Install v$_VERSION local build (also works)"
 rm -rf hello_world_normanius.egg-info
 pip uninstall -y hello_world_normanius
 pip install --no-deps "dist/hello_world_normanius-$_VERSION.tar.gz"
+python -c "$_CODE"
